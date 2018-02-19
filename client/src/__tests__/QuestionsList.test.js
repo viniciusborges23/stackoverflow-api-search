9a216475d9a0062840839590b52b1519ce324a33
Vinicius Borges <viniciusborges23@gmail.com>
2018-02-18 21:41:41 -0300

    improves unit testing

--- client/src/__tests__/QuestionsList.test.js
+++ client/src/__tests__/QuestionsList.test.js
@@ -7,16 +7,39 @@ import Loading from '../components/Loading';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe.only('<QuestionsList />', () => {
-  test('should show loading', function() {
+describe('<QuestionsList />', () => {
+  test('should show loading', () => {
     const wrapper = shallow(<QuestionsList didSearch={true} loading={true} questions={[]} />);
     expect(wrapper.find('Loading').length).toBe(1);
   });
 
-  test('should show no results', function() {
+  test('should show no results', () => {
     const wrapper = shallow(<QuestionsList didSearch={true} loading={false} questions={[]} />);
     expect(wrapper.text()).toBe(
       "You're the first one to have this idea!Sorry, we couldn't find any results matching the selected tags and filters."
     );
+  });
+
+  test('question item', () => {
+    const questions = [
+      {
+        tags: ['javascript', 'html'],
+        title: 'How to display an element inline that is located in another place in DOM?',
+        question_id: 48858187,
+        score: 0,
+        view_count: 6,
+        answer_count: 0,
+        creation_date: 1519000607,
+        link:
+          'https://stackoverflow.com/questions/48858187/how-to-display-an-element-inline-that-is-located-in-another-place-in-dom',
+        owner: {
+          profile_image: 'https://www.gravatar.com/avatar/a882f6a0a82864cbd7690e12aa766f83?s=128&d=identicon&r=PG&f=1',
+          display_name: 'Dariush',
+          link: 'https://stackoverflow.com/users/4855256/dariush'
+        }
+      }
+    ];
+    const wrapper = shallow(<QuestionsList didSearch={true} loading={false} questions={questions} />);
+    expect(wrapper.find('.list-group-item').length).toBe(1);
   });
 });
